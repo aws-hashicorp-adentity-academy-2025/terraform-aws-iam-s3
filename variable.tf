3881ecee16fa69dabf6763fbcf4b3306a3dcf046
Great-Stone <hahohh@gmail.com>
2025-06-03 17:27:38 +0900

    variable.tf 파일에서 정책 유형 변수의 기본값을 제거하여 유연성을 높임.

--- variable.tf
+++ variable.tf
@@ -6,5 +6,4 @@ variable "postfix" {
 variable "policy_type" {
   type        = string
   description = "Policy type [ s3_readonly_policy / s3_readwrite_policy ]"
-  default     = "s3_readonly_policy"
 }
