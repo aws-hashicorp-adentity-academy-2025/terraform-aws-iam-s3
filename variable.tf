a22115a2240fd37b0fe8ff51ae60be8af4300acc
Great-Stone <hahohh@gmail.com>
2025-06-03 17:25:38 +0900

    main.tf 파일에서 S3 읽기 전용 및 읽기/쓰기 IAM 사용자 정책을 로컬 변수로 정의하고, 정책 유형에 따라 동적으로 적용되도록 수정. variable.tf 파일에 정책 유형 변수를 추가하여 기본값을 설정.

--- variable.tf
+++ variable.tf
@@ -2,3 +2,9 @@ variable "postfix" {
   type        = string
   description = "Postfix for the admin user"
 }
+
+variable "policy_type" {
+  type        = string
+  description = "Policy type [ s3_readonly_policy / s3_readwrite_policy ]"
+  default     = "s3_readonly_policy"
+}
