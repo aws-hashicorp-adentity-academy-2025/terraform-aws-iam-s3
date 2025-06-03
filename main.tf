343a79260017fdf7d7c2b5332d35b477e5b503b8
Great-Stone <hahohh@gmail.com>
2025-06-03 17:06:49 +0900

    main.tf 파일에서 S3 읽기 전용 IAM 사용자 정책을 aws_iam_user_policy로 변경하고, 정책 내용을 JSON 형식으로 정의하여 권한을 세부적으로 설정.

--- main.tf
+++ main.tf
@@ -2,9 +2,28 @@ resource "aws_iam_user" "s3_readonly" {
   name = "s3-readonly-user-${var.postfix}"
 }
 
-resource "aws_iam_user_policy_attachment" "s3_readonly_attach" {
-  user       = aws_iam_user.s3_readonly.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+resource "aws_iam_user_policy" "lb_ro" {
+  name = "test"
+  user = aws_iam_user.s3_readonly.name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:Get*",
+          "s3:List*",
+          "s3:Describe*",
+          "s3-object-lambda:Get*",
+          "s3-object-lambda:List*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 output "iam_user_name" {
